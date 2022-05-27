4b95cb6fc8a7662bf94dc84372b1b6e8d622abb7
poseidon <61605433+poseidonKey@users.noreply.github.com>
2022-05-27 20:53:52 +0900

    first time

---
+++ android/app/src/main/kotlin/com/example/bbanto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bbanto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "BBANTO",
+      home: Grade(),
+    );
+  }
+}
+
+class Grade extends StatelessWidget {
+  const Grade({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber.shade800,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("BBANTO"),
+        backgroundColor: Colors.amber.shade700,
+        centerTitle: true,
+      ),
+    );
+  }
+}
